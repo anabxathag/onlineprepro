1759df0433c110682f309c4fad405f6b8f0b5744
anabxathag <109953263+anabxathag@users.noreply.github.com>
2022-07-29 08:53:42 +0700

    change

--- cyclelocker.py
+++ cyclelocker.py
@@ -12,13 +12,13 @@ def main():
     diff_10 = abs(re_10 - fa_10)
     diff_1 = abs(re_1 - fa_1)
     if diff_1000 >= 5:
-        diff_1000 = 9 - diff_1000
+        diff_1000 = 10 - diff_1000
     if diff_100 >= 5:
-        diff_100 = 9 - diff_100
+        diff_100 = 10 - diff_100
     if diff_10 >= 5:
-        diff_10 = 9 - diff_10
+        diff_10 = 10 - diff_10
     if diff_1 >= 5:
-        diff_1 = 9 - diff_1
+        diff_1 = 10 - diff_1
     tol_min = diff_1000 + diff_100 + diff_10 + diff_1
     print(tol_min)
 main()
